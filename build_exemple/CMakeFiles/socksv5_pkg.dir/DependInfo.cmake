4564654b3253537f8cbbda77f88545759e3df20d
Corentin Sanchez <corentin.sanchez@hds.utc.fr>
2024-12-17 02:02:41 +0100

    [challenge] add build_exemple folder : montre mon résultat

---
+++ build_exemple/CMakeFiles/socksv5_pkg.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sancheco/thales_ws/socksv5_project/socks5_server_pkg/socks5_srv/main.cpp" "/home/sancheco/thales_ws/socksv5_project/socks5_server_pkg/build_exemple/CMakeFiles/socksv5_pkg.dir/main.cpp.o"
+  "/home/sancheco/thales_ws/socksv5_project/socks5_server_pkg/socks5_srv/socks_client.cpp" "/home/sancheco/thales_ws/socksv5_project/socks5_server_pkg/build_exemple/CMakeFiles/socksv5_pkg.dir/socks_client.cpp.o"
+  "/home/sancheco/thales_ws/socksv5_project/socks5_server_pkg/socks5_srv/socks_server.cpp" "/home/sancheco/thales_ws/socksv5_project/socks5_server_pkg/build_exemple/CMakeFiles/socksv5_pkg.dir/socks_server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build_exemple/CMakeFiles/socksv5_pkg.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/socksv5_pkg.dir/main.cpp.o"
+  "CMakeFiles/socksv5_pkg.dir/socks_client.cpp.o"
+  "CMakeFiles/socksv5_pkg.dir/socks_server.cpp.o"
+  "socksv5_pkg"
+  "socksv5_pkg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/socksv5_pkg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
